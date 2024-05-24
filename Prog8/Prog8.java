0f707559d7a5102edd1f879f878a72adbf79a3c1
Kritika <130730571+Kriti1211@users.noreply.github.com>
2024-05-24 11:54:01 +0530

    Update Prog8.java

--- Prog8/Prog8.java
+++ Prog8/Prog8.java
@@ -1,3 +1,10 @@
+/*
+Write a program that uses Java Swing and JDBC to create a stand-alone application:
+Create two tables namely, Representative (RepNo, RepName, State, Comission, Rate) and
+Customer (CustNo, CustName, State, Credit_Limit, RepNo) in MySQL database.
+Use appropriate Swing components to insert values in a form.
+Display in frame those customers and representatives belonging to a particular state.
+*/
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
