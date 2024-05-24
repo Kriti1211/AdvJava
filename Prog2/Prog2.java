87e5a05ac8bb42609506447fd477c8cdbc133973
Kritika <130730571+Kriti1211@users.noreply.github.com>
2024-05-24 11:50:54 +0530

    Update Prog2.java

--- Prog2/Prog2.java
+++ Prog2/Prog2.java
@@ -1,4 +1,13 @@
-
+/*
+Write a Program that simulates a telephone that records missed incoming calls in
+lists. Maintain the phone callers list in hashmap . For each missed call , telephone
+number of origin, and name of the caller if the name is available. For unlisted
+numbers, set the name to “private caller”. Missed calls have to be maintained in the
+order they arrive. User goes through the missed call list and can decide to delete the
+call or display the call details.
+Additional: Display all the phone numbers from the callers list whose name start
+from “A”.
+*/
 import java.util.*;
 
 class Contact {
